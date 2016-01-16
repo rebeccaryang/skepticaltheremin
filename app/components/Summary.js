b74e9554871c9cccd7cbcf3c9d7c831d9f70ec01
Stephen Saunders <saunders.stephen@gmail.com>
2016-01-15 19:38:41 -0800

    Add Summary components and connect to state

--- app/components/Summary.js
+++ app/components/Summary.js
@@ -1,15 +1,30 @@
+'use strict';
+
 var React = require('react');
+var ReactDOM = require('react-dom');
 var RacerList = require('./RacerList');
 var NavBar = require('./NavBar');
+var { map, extend } = require('lodash');
 
 var Summary = React.createClass({
+
 	render: function(){
+    let { name, results } = this.props.activeRace;
+
+    results = map([{},{},{},{}], function(racer, index){
+      return extend({}, racer, {
+        key: index,
+        rank: index,
+        name: 'Racer Name'
+      });
+    });
+
 		return (
 			<div>
-        <h2>Results of {this.props.activeRace.name}:</h2>
-        <RacerList racers={this.props.activeRace.results} />
+        <h2>Results of {name}:</h2>
+        <RacerList racers={results} />
       </div>
-		)
+		);
 	}
 });
 
